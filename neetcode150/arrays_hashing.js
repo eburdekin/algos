e98f5f3c91b20481410c18a1ac1206e28522c83c
Eileen <eileenburdekin@proton.me>
2024-03-20 11:52:20 -0700

    alt solution for contains duplicate

--- neetcode150/arrays_hashing.js
+++ neetcode150/arrays_hashing.js
@@ -1,8 +1,10 @@
 // EASY
 
 // Contains Duplicate
+//// Sets store collections of unique values
 
 var containsDuplicate = (nums) => {
+  // faster but takes up more memory
   const set = new Set(nums);
   const isEqual = set.size === nums.length;
 
@@ -10,11 +12,26 @@ var containsDuplicate = (nums) => {
 };
 
 var containsDuplicateEarlyExit = function (nums) {
+  // slower but takes up less memory due to early exit
   const set = new Set();
 
   for (const n of nums) {
     if (set.has(n)) return true;
     set.add(n);
   }
+  return false;
+};
+
+//// instead of Set, store key-value pairs of char:1 in cache object
+
+var containsDuplicate = function (nums) {
+  const cache = {};
+
+  for (const num of nums) {
+    if (num in cache) return true;
+
+    cache[num] = 1;
+  }
+
   return false;
 };
