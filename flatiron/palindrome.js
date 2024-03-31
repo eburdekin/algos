d48525bbc468b50dba49d65cb169fd0a5848b9f4
Eileen <eileenburdekin@proton.me>
2024-03-30 20:01:11 -0700

    simplify palindrome

--- flatiron/palindrome.js
+++ flatiron/palindrome.js
@@ -1,17 +1,19 @@
 function reverseString(word) {
-  // create an array from the input string
-  const wordArray = word.split("");
-  // reverse the array
-  const reversedArray = wordArray.reverse();
-  // create a string for the reversed array
-  const reversedWord = reversedArray.join("");
-  // return the reversed string
-  return reversedWord;
+  // // create an array from the input string
+  // const wordArray = word.split("");
+
+  // // reverse the array
+  // const reversedArray = wordArray.reverse();
+
+  // // create a string for the reversed array
+  // const reversedWord = reversedArray.join("");
+
+  const reversedString = word.split("").reverse().join("");
+  return reversedString;
 }
 
 function isPalindrome(word) {
-  const drow = reverseString(word);
-  return drow === word;
+  return reverseString(word) === word;
 }
 
 if (require.main === module) {
